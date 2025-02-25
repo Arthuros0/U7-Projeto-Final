caedba3dcce8faf51e9f28eab1912fecb5a32284
Arthur Moreira <arthur_moreira.10@hotmail.com>
2025-02-25 20:27:01 -0300

    Adiciona biblioteca do joystick

---
+++ joystick.c
@@ -0,0 +1,58 @@
+#include "joystick.h"
+#include <stdio.h>
+
+bool menu_estufas=false;
+bool sub_menu_estufas=false;
+
+uint8_t indice_menu=0;
+
+absolute_time_t debounce_joy;
+
+
+void joystick_setup(){
+  adc_init();
+  adc_gpio_init(Y_JOY);
+  adc_gpio_init(X_JOY);
+}
+
+int64_t joy_callback(alarm_id_t id, void *user_data){
+  if (menu_estufas)
+  {
+    if(time_reached(debounce_joy)){
+      adc_select_input(Y_CHANNEL);
+      if(!sub_menu_estufas){
+        if ((adc_read()) > MAX_JOY)
+        {
+          indice_menu--;
+          if(indice_menu>2)indice_menu=2;
+          debounce_joy=delayed_by_ms(get_absolute_time(),200);
+          return 200000;
+        }else if ((adc_read()) < MIN_JOY)
+        {
+          indice_menu++;
+          if(indice_menu>2)indice_menu=0;
+          debounce_joy=delayed_by_ms(get_absolute_time(),200);
+          return 200000;
+        }
+      }
+      adc_select_input(X_CHANNEL);
+
+      if ((adc_read()) > MAX_JOY)
+      {
+        sub_menu_estufas=true;
+        debounce_joy=delayed_by_ms(get_absolute_time(),200);
+        return 200000;
+      }else if ((adc_read()) < MIN_JOY)
+      {
+        sub_menu_estufas=false;
+        debounce_joy=delayed_by_ms(get_absolute_time(),200);
+        return 200000;
+      }
+
+    }
+    return 250000;
+  
+  }
+  return 0;
+}
+
---
+++ joystick.h
@@ -0,0 +1,20 @@
+#include "hardware/adc.h"
+#include "pico/stdlib.h"
+#include "estufa.h"
+
+#define Y_JOY 26  
+#define X_JOY 27  
+#define Y_CHANNEL 0 //Canal do ADC vinculado ao Y_JOY
+#define X_CHANNEL 1 //Canal do ADC vinculado ao X_JOY
+
+#define MAX_JOY 3700
+#define MIN_JOY 400
+
+extern bool menu_estufas;
+extern bool sub_menu_estufas;
+extern uint8_t indice_menu;
+extern absolute_time_t debounce_joy;
+
+
+int64_t joy_callback(alarm_id_t id, void *user_data);
+void joystick_setup();
