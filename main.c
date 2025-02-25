47627c32396ef7b6c1be635f6246aa34267b0ec6
Arthur Moreira <arthur_moreira.10@hotmail.com>
2025-02-25 20:30:33 -0300

    Adiciona lógica dos botões

--- main.c
+++ main.c
@@ -3,13 +3,17 @@
 #include "pico/bootrom.h"
 #include "ssd1306.h"
 #include "estufa.h"
+#include "joystick.h"
 
 #define I2C_PORT i2c1
 #define I2C_SDA 14
 #define I2C_SCL 15
 #define ENDERECO 0x3C
 
+#define BUTTON_A 5
 #define BUTTON_B 6
+#define BUTTON_JOY 22
+
 
 ssd1306_t ssd; //Váriavel do tipo ssd1306_t, armazena as informações relacionados ao display
 
@@ -25,20 +29,23 @@ int main()
     i2c_init(I2C_PORT,400*1000);
     init_i2c_pins(I2C_SDA,I2C_SCL);
 
+    init_button(BUTTON_A);
     init_button(BUTTON_B);
+    init_button(BUTTON_JOY);
 
+    joystick_setup();
     gpio_set_irq_enabled_with_callback(BUTTON_B,GPIO_IRQ_EDGE_FALL,true,&button_callback);
+    gpio_set_irq_enabled_with_callback(BUTTON_JOY,GPIO_IRQ_EDGE_FALL,true,&button_callback);
 
     init_display(&ssd,ENDERECO,I2C_PORT);
 
     button_debounce=delayed_by_ms(get_absolute_time(), 200);
 
 
-    bool cor=false;
     while (true) {
         cor=!cor;
-        printf("Hello, world!\n");
-        sleep_ms(5000);
+        desenha_menu(&ssd);
+        sleep_ms(1000);
     }
 }
 
@@ -53,7 +60,11 @@ void button_callback(uint gpio,uint32_t events){
     if (time_reached(button_debounce)) //Verifica se o tempo de debounce foi atigindo
     {
         if(gpio == BUTTON_B){
-        reset_usb_boot(0, 0);
+            reset_usb_boot(0, 0);
+        }else if(gpio == BUTTON_JOY){
+            menu_estufas=!menu_estufas;
+            add_alarm_in_ms(100,joy_callback,NULL, false);
+            debounce_joy=delayed_by_ms(get_absolute_time(),200);
         }
         button_debounce=delayed_by_ms(get_absolute_time(), 200); //Atualiza debounce
     }
